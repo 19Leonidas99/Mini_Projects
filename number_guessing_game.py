a7c6b8f7f85ce7a087d67bf9c55131415ff7ba2e
MAYCON BALDOINO <mm3432@live.mdx.ac.uk>
2023-04-25 20:46:48 +0100

    # add a high score

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -3,22 +3,25 @@ import random
 def guess_the_nunber():
     guessing_number = random.randint(0, 100)
     number_of_guesses = 0
-    print(guessing_number)
+    print(guessing_number) # Just for testing
     
     print('Guess The Number Game!\nGuess the random number between 0-100,\nlesser guesses better. Simple right?!')
-    guess = int(input('Guess: '))
     
-    if guess == guessing_number:
-        print('Gongrats!!! You Won')
-        quit()
-    
-    elif guess < guessing_number:
-        print('It is a higher number')
-        number_of_guesses += 1
-    
-    else:
-        print('Lower')
-        number_of_guesses += 1
+    playing = True
+    while playing:
+        guess = int(input('Guess: '))
+        
+        if guess == guessing_number:
+            print('Gongrats!!! You Won\nWith', number_of_guesses, 'guesses' )
+            quit()
+        
+        elif guess < guessing_number:
+            print('It is a higher number')
+            number_of_guesses += 1
+        
+        else:
+            print('Lower')
+            number_of_guesses += 1
         
  
 guess_the_nunber()
